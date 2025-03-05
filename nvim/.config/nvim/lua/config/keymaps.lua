000dff50c5970dee04c116ede0a4beacdc5387a3
Yousef Hadder <yousefhadder@github.com>
2025-03-05 20:46:19 +0300

    Update keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,9 @@ local opts = { noremap = true, silent = true }
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", opts)
+
+-- move text up and down
+keymap.set("n", "<a-j>", "<esc>:m .+1<cr>==gi", opts) -- Alt-j
+keymap.set("n", "<a-k>", "<esc>:m .-2<cr>==gi", opts) -- Alt-k
+
+keymap.set("n", "<leader>r", ":luafile $MYVIMRC<CR>", { noremap = true, silent = true })
