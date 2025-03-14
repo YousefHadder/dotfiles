d78802d8048e635d3129c01c28e5f27ee2b9830a
${GIT_AUTHOR_NAME} <${GIT_AUTHOR_EMAIL}>
2025-03-14 09:26:46 -0500

    Add harpoon

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,13 @@
 -- Add any additional keymaps here
 
 local keymap = vim.keymap
+local api = vim.api
 local opts = { noremap = true, silent = true }
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
 -- Disable arrow keys in normal mode
 keymap.set("n", "<Up>", "<nop>", { noremap = true, silent = true })
@@ -20,5 +26,33 @@ keymap.set("n", "<a-k>", "<esc>:m .-2<cr>==gi", opts) -- Alt-k
 
 keymap.set("n", "<leader>r", ":luafile $MYVIMRC<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+keymap.set("n", "<leader>ha", function()
+  harpoon:list():add()
+end)
+keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+keymap.set("n", "<C-1>", function()
+  harpoon:list():select(1)
+end)
+keymap.set("n", "<C-2>", function()
+  harpoon:list():select(2)
+end)
+keymap.set("n", "<C-3>", function()
+  harpoon:list():select(3)
+end)
+keymap.set("n", "<C-4>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+keymap.set("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+keymap.set("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
