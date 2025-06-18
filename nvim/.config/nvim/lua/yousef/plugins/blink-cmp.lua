757c681b767823d059f6c80da771acc0f70008e9
yousefhadder <yousefhadder@github.com>
2025-06-18 18:01:22 -0500

    updates

--- nvim/.config/nvim/lua/yousef/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/yousef/plugins/blink-cmp.lua
@@ -3,28 +3,41 @@ return { -- Autocompletion
 	event = "VimEnter",
 	version = "1.*",
 	dependencies = {
-		-- Snippet Engine
 		{
 			"L3MON4D3/LuaSnip",
 			version = "2.*",
 			build = (function()
 				return "make install_jsregexp"
 			end)(),
+			opts = {},
 			dependencies = {
-				--    https://github.com/rafamadriz/friendly-snippets
 				{
 					"rafamadriz/friendly-snippets",
 					config = function()
 						require("luasnip.loaders.from_vscode").lazy_load()
+						-- Load language-specific snippets
+						require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 					end,
 				},
+				-- Add language-specific snippet collections
+				"honza/vim-snippets", -- Additional snippet collection
 			},
-			opts = {},
+			config = function()
+				local luasnip = require("luasnip")
+				-- Enable autotrigger for snippets
+				luasnip.config.set_config({
+					enable_autosnippets = true,
+					store_selection_keys = "<Tab>",
+				})
+
+				-- Language-specific configurations
+				luasnip.filetype_extend("typescript", { "javascript" })
+				luasnip.filetype_extend("typescriptreact", { "typescript", "javascript", "javascriptreact" })
+				luasnip.filetype_extend("javascriptreact", { "javascript" })
+			end,
 		},
 		"folke/lazydev.nvim",
 	},
-	--- @module 'blink.cmp'
-	--- @type blink.cmp.Config
 	opts = {
 		keymap = {
 			-- See :h blink-cmp-config-keymap for defining your own keymap
@@ -51,13 +64,33 @@ return { -- Autocompletion
 				},
 			},
 		},
+
 		sources = {
-			default = { "lsp", "path", "snippets", "lazydev" },
+			default = { "lsp", "path", "snippets", "lazydev", "buffer" },
 			providers = {
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+				buffer = {
+					-- Buffer completion for current and visible buffers
+					opts = {
+						get_bufnrs = function()
+							local bufs = {}
+							for _, win in ipairs(vim.api.nvim_list_wins()) do
+								bufs[vim.api.nvim_win_get_buf(win)] = true
+							end
+							return vim.tbl_keys(bufs)
+						end,
+					},
+				},
+			},
+			-- Language-specific source configuration
+			per_filetype = {
+				lua = { "lsp", "lazydev", "snippets", "path" },
+				typescript = { "lsp", "snippets", "path", "buffer" },
+				javascript = { "lsp", "snippets", "path", "buffer" },
+				ruby = { "lsp", "snippets", "path", "buffer" },
+				go = { "lsp", "snippets", "path", "buffer" },
 			},
 		},
-
 		snippets = { preset = "luasnip" },
 
 		-- See :h blink-cmp-config-fuzzy for more information
--- nvim/.config/nvim/lua/yousef/plugins/copilot.lua
+++ nvim/.config/nvim/lua/yousef/plugins/copilot.lua
@@ -2,7 +2,6 @@ return {
   "github/copilot.vim",
   event = "InsertEnter",
   config = function()
-    vim.g.copilot_no_tab_map = true
     vim.g.copilot_filetypes = {
       ["go"] = true,
       ["python"] = true,
