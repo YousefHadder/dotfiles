201f2d557db4ff98577d015731ff6d0c38c53ca1
yousefhadder <yousefhadder@github.com>
2025-07-13 19:34:20 -0500

    keymaps update

--- nvim/.config/nvim/lua/yousef/config/keymaps.lua
+++ nvim/.config/nvim/lua/yousef/config/keymaps.lua
@@ -1,108 +1,113 @@
-local keymap = vim.keymap
+local keymap = vim.keymap.set
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
-keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+keymap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- REQUIRED
-keymap.set("i", "jk", "<ESC>", { noremap = true, desc = "Exit insert mode with jk" })
+-- Use jk instead of Esc
+keymap("i", "jk", "<ESC>")
 
--- Clear search highlight
-keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+-- Reload
+keymap("n", "<leader>rf", "<cmd> so % <CR>", { silent = true })
 
--- Disable arrow keys in normal mode
-keymap.set("n", "<Up>", "<nop>", { noremap = true, silent = true })
-keymap.set("n", "<Down>", "<nop>", { noremap = true, silent = true })
-keymap.set("n", "<Left>", "<nop>", { noremap = true, silent = true })
-keymap.set("n", "<Right>", "<nop>", { noremap = true, silent = true })
+-- Select All
+keymap("n", "vag", "<cmd>keepjumps normal! ggVG<cr>")
 
--- Select all
-keymap.set("n", "vag", "ggVG")
+-- Better up/down movement
+keymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
--- -- move text up and down
-keymap.set({ "n", "v" }, "<a-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) -- Alt-k
-keymap.set({ "n", "v" }, "<a-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) -- Alt-j
+-- Clear search highlighting
+keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- source file
-keymap.set("n", "<leader>so", "<cmd> so % <CR>", { noremap = true, silent = true, desc = "Source file" })
+-- Better indenting
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
--- save file
-keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+-- Move text up and down
+keymap({ "n", "v" }, "<a-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true }) -- Alt-k
+keymap({ "n", "v" }, "<a-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true }) -- Alt-j
 
--- save file without auto-formatting
-keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", { noremap = true, silent = true })
+-- Keep cursor centered when scrolling
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
--- quit file
-keymap.set("n", "<C-q>", "<cmd> q <CR>", { noremap = true, silent = true })
+-- Keep cursor centered when searching
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
 
--- delete single character without copying into register
-keymap.set("n", "x", '"_x', { noremap = true, silent = true })
-
--- Vertical scroll and center
-keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-
--- split window commands
-keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { noremap = true, silent = true }) -- vertical split
-keymap.set("n", "<leader>-", "<cmd>split<CR>", { noremap = true, silent = true })  -- horizontal split
-
--- Move splits
-keymap.set('n', '<leader>ml', '<C-w>H', { desc = 'Move split left' })
-keymap.set('n', '<leader>md', '<C-w>J', { desc = 'Move split down' })
-keymap.set('n', '<leader>mu', '<C-w>K', { desc = 'Move split up' })
-keymap.set('n', '<leader>mr', '<C-w>L', { desc = 'Move split right' })
-
--- Navigate between splits
-keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true })
-keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true })
-keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true })
-keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true })
-
--- Find and center
-keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
-keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
-
--- Resize with arrows
-keymap.set("n", "<Up>", ":resize -2<CR>", { noremap = true, silent = true })
-keymap.set("n", "<Down>", ":resize +2<CR>", { noremap = true, silent = true })
-keymap.set("n", "<Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
-keymap.set("n", "<Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
-
--- Navigate between buffers in order
-keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
-keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { silent = true })
-
-keymap.set("n", "<leader>bd", ":bdelete!<CR>", { noremap = true, silent = true })   -- close buffer
-keymap.set("n", "<leader>bn", "<cmd> enew <CR>", { noremap = true, silent = true }) -- new buffer
-
--- Toggle line wrapping
-keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", { noremap = true, silent = true })
-
--- Stay in indent mode
-keymap.set("v", "<", "<gv", { noremap = true, silent = true })
-keymap.set("v", ">", ">gv", { noremap = true, silent = true })
+-- Better paste (doesn't replace clipboard)
+keymap("x", "<leader>p", [["_dP]])
 
 -- remember yanked
-keymap.set("v", "p", '"_dp', { noremap = true, silent = true })
+keymap("v", "p", '"_dp', { noremap = true, silent = true })
 
-keymap.set('i', '<C-w>', '<Plug>(copilot-accept-word)')
-keymap.set('i', '<C-l>', '<Plug>(copilot-accept-line)')
-keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
+-- delete single character without copying into register
+keymap("n", "x", '"_x', { noremap = true, silent = true })
 
-keymap.set(
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Resize with arrows
+keymap("n", "<Up>", ":resize -2<CR>", { noremap = true, silent = true })
+keymap("n", "<Down>", ":resize +2<CR>", { noremap = true, silent = true })
+keymap("n", "<Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+keymap("n", "<Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+
+-- Buffer navigation
+keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
+keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { silent = true })
+keymap("n", "<leader>bd", ":bdelete!<CR>", { noremap = true, silent = true })   -- close buffer
+keymap("n", "<leader>bn", "<cmd> enew <CR>", { noremap = true, silent = true }) -- new buffer
+
+-- Tab navigation
+keymap("n", "[t", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+keymap("n", "]t", "<cmd>tabnext<CR>", { desc = "Next tab" })
+keymap("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close tab" })
+
+-- split window commands
+keymap("n", "<leader>|", "<cmd>vsplit<CR>", { noremap = true, silent = true }) -- vertical split
+keymap("n", "<leader>-", "<cmd>split<CR>", { noremap = true, silent = true })  -- horizontal split
+
+-- Move splits
+keymap('n', '<leader>ml', '<C-w>H', { desc = 'Move split left' })
+keymap('n', '<leader>md', '<C-w>J', { desc = 'Move split down' })
+keymap('n', '<leader>mu', '<C-w>K', { desc = 'Move split up' })
+keymap('n', '<leader>mr', '<C-w>L', { desc = 'Move split right' })
+
+-- Navigate between splits
+keymap("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true })
+keymap("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true })
+keymap("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true })
+keymap("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true })
+
+-- Terminal
+keymap("n", "<leader>tt", "<cmd>terminal<CR>", { desc = "Open terminal" })
+keymap("n", "<leader>ts", "<cmd>split | terminal<CR>", { desc = "Open terminal in split" })
+keymap("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", { desc = "Open terminal in vertical split" })
+keymap("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Quick save and quit
+keymap("n", "<leader>wa", "<cmd>write<CR>", { desc = "Save file" })
+keymap("n", "<leader>q", "<cmd>quit<CR>", { desc = "Quit" })
+keymap("n", "<leader>Q", "<cmd>qall<CR>", { desc = "Quit all" })
+
+keymap(
   "n",
   "<leader>rw",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = "Replace word cursor is on globally" }
 )
 
-keymap.set("n", "<leader>fp", function()
+keymap("n", "<leader>cp", function()
   local filePath = vim.fn.expand("%:~")                -- Gets the file path relative to the home directory
   vim.fn.setreg("+", filePath)                         -- Copy the file path to the clipboard register
   print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
 end, { desc = "Copy file path to clipboard" })
+
 
 local function clean_lsp_mappings()
   -- Remove custom LSP mappings that conflict with defaults
