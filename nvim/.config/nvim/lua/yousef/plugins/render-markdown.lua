7247ca424703d1e29598c4820e87a4e70e5f844e
yousefhadder <yousefhadder@github.com>
2025-08-17 15:45:41 -0500

    change code block color in markdown.

--- nvim/.config/nvim/lua/yousef/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/yousef/plugins/render-markdown.lua
@@ -5,9 +5,24 @@ return {
     require('render-markdown').setup({
       completion = {
         blink = {
-          enagled = true,
+          enabled = true,
         },
       },
+    })
+
+    -- Customize colors for code blocks and inline code
+    local function set_render_markdown_hl()
+      local ns = 0
+      -- Fenced code blocks
+      vim.api.nvim_set_hl(ns, 'RenderMarkdownCode', { fg = '#c0caf5', bg = '#1f2335' })
+      -- Inline `code`
+      vim.api.nvim_set_hl(ns, 'RenderMarkdownCodeInline', { fg = '#c0caf5', bg = '#1f2335' })
+    end
+
+    set_render_markdown_hl()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('RenderMarkdownHL', { clear = true }),
+      callback = set_render_markdown_hl,
     })
   end,
 }
