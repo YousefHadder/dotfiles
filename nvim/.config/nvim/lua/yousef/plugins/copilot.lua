9d065fa7c506d8949dfb51011ca5b9110f0c33ff
yousefhadder <yousefhadder@github.com>
2025-09-25 17:37:48 -0500

    fix

--- nvim/.config/nvim/lua/yousef/plugins/copilot.lua
+++ nvim/.config/nvim/lua/yousef/plugins/copilot.lua
@@ -1,45 +1,40 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  dependencies = {
-    "zbirenbaum/copilot-cmp",
-  },
-  config = function()
-    require("copilot").setup({
-      panel = {
-        keymap = {
-          accept = "<S-Tab>",
-        },
-      },
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<S-Tab>",
-          accept_word = "<C-w>",
-          accept_line = "<C-l>",
-        },
-      },
-    })
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	dependencies = {
+		"zbirenbaum/copilot-cmp",
+	},
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<S-Tab>",
+					accept_word = "<C-w>",
+					accept_line = "<C-l>",
+				},
+			},
+		})
 
-    -- Hide Copilot ghost text while Blink's menu is visible
-    local grp = vim.api.nvim_create_augroup("CopilotBlinkHarmony", { clear = true })
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "BlinkCmpMenuOpen",
-      group = grp,
-      callback = function()
-        require("copilot.suggestion").dismiss()
-      end,
-    })
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "BlinkCmpMenuClose",
-      group = grp,
-      callback = function()
-        -- Re-enable copilot suggestions after blink menu closes
-        vim.defer_fn(function()
-          require("copilot.suggestion").next()
-        end, 100)
-      end,
-    })
-  end,
+		-- Hide Copilot ghost text while Blink's menu is visible
+		local grp = vim.api.nvim_create_augroup("CopilotBlinkHarmony", { clear = true })
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "BlinkCmpMenuOpen",
+			group = grp,
+			callback = function()
+				require("copilot.suggestion").dismiss()
+			end,
+		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "BlinkCmpMenuClose",
+			group = grp,
+			callback = function()
+				-- Re-enable copilot suggestions after blink menu closes
+				vim.defer_fn(function()
+					require("copilot.suggestion").next()
+				end, 100)
+			end,
+		})
+	end,
 }
