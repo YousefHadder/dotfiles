cd5a657bce6fdb0ac259ead9583a8826532a2f70
yousefhadder <yousefhadder@github.com>
2025-11-01 15:55:56 -0500

    use snacks.gh

--- nvim/.config/nvim/lua/yousef/plugins/snacks.lua
+++ nvim/.config/nvim/lua/yousef/plugins/snacks.lua
@@ -33,11 +33,27 @@ return {
 				},
 			},
 		},
+		gh = {
+			-- your gh configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		picker = {
+			sources = {
+				gh_issue = {
+					-- your gh_issue picker configuration comes here
+					-- or leave it empty to use the default settings
+				},
+				gh_pr = {
+					-- your gh_pr picker configuration comes here
+					-- or leave it empty to use the default settings
+				},
+			},
+		},
 		notifier = {
 			enabled = true,
 			timeout = 3000,
 		},
-		picker = { enabled = false }, -- Disabled in favor of fzf-lua
 		quickfile = { enabled = true },
 		scope = { enabled = true },
 		-- scroll = { enabled = true },
@@ -50,6 +66,35 @@ return {
 		},
 	},
 	keys = {
+		{
+			"<leader>gi",
+			function()
+				Snacks.picker.gh_issue()
+			end,
+			desc = "GitHub Issues (open)",
+		},
+		{
+			"<leader>gI",
+			function()
+				Snacks.picker.gh_issue({ state = "all" })
+			end,
+			desc = "GitHub Issues (all)",
+		},
+		{
+			"<leader>gp",
+			function()
+				Snacks.picker.gh_pr()
+			end,
+			desc = "GitHub Pull Requests (open)",
+		},
+		{
+			"<leader>gP",
+			function()
+				Snacks.picker.gh_pr({ state = "all" })
+			end,
+			desc = "GitHub Pull Requests (all)",
+		},
+
 		-- Explorer
 		{
 			"<leader>e",
