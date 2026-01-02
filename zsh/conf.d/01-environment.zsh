8cb5a5d293fedf49240607a2ce8f182805e377d9
yousefhadder <yousefhadder@github.com>
2026-01-02 14:32:02 -0600

    updates

--- zsh/conf.d/01-environment.zsh
+++ zsh/conf.d/01-environment.zsh
@@ -3,6 +3,8 @@
 export ZSH="$HOME/.oh-my-zsh"
 export GHOSTTY_HOME=$HOME/Library/Application\ Support/com.mitchellh.ghostty/config
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_STATE_HOME="$HOME/.config"
+
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export STARSHIP_CONFIG=$HOME/.config/starship.toml
