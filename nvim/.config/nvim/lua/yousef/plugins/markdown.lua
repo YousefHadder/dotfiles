9df4d56541261df7aa2fbc0acdb83fee17f4d1cc
yousefhadder <yousefhadder@github.com>
2026-04-12 10:03:24 -0500

    updates

--- nvim/.config/nvim/lua/yousef/plugins/markdown.lua
+++ nvim/.config/nvim/lua/yousef/plugins/markdown.lua
@@ -2,7 +2,95 @@ local use_local_markdown_plus = true
 
 local markdown_plus_spec = {
 	ft = { "markdown", "csv" },
-	opts = {},
+	opts = {
+		enabled = true,
+
+		-- Toggle individual feature modules
+		features = {
+			list_management = true,
+			text_formatting = true,
+			thematic_break = true,
+			links = true,
+			images = true,
+			headers_toc = true,
+			quotes = true,
+			callouts = true,
+			code_block = true,
+			html_block_awareness = true,
+			table = true,
+			footnotes = true,
+		},
+
+		-- Global keymap toggle (all <Plug> defaults)
+		keymaps = {
+			enabled = true,
+		},
+
+		-- Filetypes that trigger the plugin
+		filetypes = { "markdown" },
+
+		-- Table of Contents
+		toc = {
+			initial_depth = 4, -- Minimum heading depth to include (2 = skip H1)
+		},
+
+		-- Table editing
+		table = {
+			enabled = true,
+			auto_format = true, -- Re-align on <Tab>/<CR>
+			default_alignment = "left", -- "left" | "center" | "right"
+			confirm_destructive = true, -- Prompt before deleting rows/cols
+			keymaps = {
+				enabled = true,
+				prefix = "<localleader>t", -- All table maps start with this
+				insert_mode_navigation = true, -- Tab/S-Tab navigate cells
+			},
+		},
+
+		-- Callout blocks (> [!NOTE], etc.)
+		callouts = {
+			default_type = "NOTE", -- Default callout when inserting
+			custom_types = {}, -- Add custom callout types here
+		},
+
+		-- Thematic breaks (horizontal rules)
+		thematic_break = {
+			style = "---", -- "---" | "***" | "___"
+		},
+
+		-- Fenced code blocks
+		code_block = {
+			enabled = true,
+			fence_style = "backtick", -- "backtick" | "tilde"
+			languages = { "lua", "python", "javascript", "typescript", "bash", "json", "yaml", "markdown" },
+		},
+
+		-- Footnotes
+		footnotes = {
+			section_header = "Footnotes", -- Header text for footnotes section
+			confirm_delete = true, -- Prompt before deleting footnotes
+		},
+
+		-- List management
+		list = {
+			smart_outdent = true, -- Outdent empty list items instead of removing
+			checkbox_completion = {
+				enabled = false, -- Add completion metadata to checked items
+				format = "emoji", -- "emoji" | "text"
+				date_format = "%Y-%m-%d",
+				remove_on_uncheck = true,
+				update_existing = true,
+			},
+		},
+
+		-- Link management
+		links = {
+			smart_paste = {
+				enabled = true, -- Auto-fetch URL titles on paste
+				timeout = 5, -- Fetch timeout in seconds
+			},
+		},
+	},
 }
 
 if use_local_markdown_plus then
